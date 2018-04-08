f7c9dba93759f474f872c784c053865e402d4956
Kyle Brown <brow1144@purdue.edu>
2018-04-08 13:32:13 -0400

    Ugly Sign up UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-import { Input, Button } from 'mdbreact';
-import { Row, Col } from 'reactstrap';
+import {Input, Button, Card, CardImage, CardBody, CardTitle, CardText} from 'mdbreact';
+import {Form, FormGroup, Label} from 'reactstrap';
+
+import {NavLink} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Row>
-          <Col xs='0' md='2'/>
-          <Col xs='12' md='8'>
-            <h2 className="mb-5">Form login</h2>
-            <form>
-              <p className="h5 text-center mb-4">Sign in</p>
-              <Input label="Type your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
-              <Input label="Type your password" icon="lock" group type="password" validate/>
-              <div className="text-center">
-                <Button>Login</Button>
-              </div>
-            </form>
-          </Col>
-          <Col xs='0' md='2'/>
-        </Row>
+      <div className="text-center">
+        <div className="Absolute-CenterK is-ResponsiveK">
+          <Card>
+            <CardBody>
+              <Form>
+                <Label className="text">Please Sign In</Label>
+                <Input label="Email" />
+                <Input label="Password" />
+                <br/>
+                <Button color="success" block>Sign In!</Button>
+              </Form>
+              <hr/>
+              <NavLink style={{textDecoration: 'none'}} to="/create-account">
+                <Button color="success" block>Sign Up!</Button>
+              </NavLink>
+              <br/>
+            </CardBody>
+          </Card>
+        </div>
       </div>
     );
   }
