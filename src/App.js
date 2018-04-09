e7a31fe695c72115a5302f77e81583df185ba938
Kyle Brown <brow1144@purdue.edu>
2018-04-09 10:04:06 -0400

    Image on left is now set

--- src/App.js
+++ src/App.js
@@ -2,35 +2,58 @@ import React, {Component} from 'react';
 import './App.css';
 
 import {Input, Button, Card, CardImage, CardBody, CardTitle, CardText} from 'mdbreact';
-import {Form, FormGroup, Label} from 'reactstrap';
+import {Row, Col, Form, Label} from 'reactstrap';
+
+import blueImage from './blueSignIn.jpg';
 
 import {NavLink} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="text-center">
-        <div className="Absolute-CenterK is-ResponsiveK">
-          <Card>
-            <CardBody>
-              <Form>
-                <Label className="text">Please Sign In</Label>
-                <Input label="Email" />
-                <Input label="Password" />
-                <br/>
-                <Button color="success" block>Sign In!</Button>
-              </Form>
-              <hr/>
-              <NavLink style={{textDecoration: 'none'}} to="/create-account">
-                <Button color="success" block>Sign Up!</Button>
-              </NavLink>
-              <br/>
-            </CardBody>
-          </Card>
+      <section className="container">
+        <div className="left-half">
+          {/*<img src={blueImage} alt="Blue Blob"/>*/}
         </div>
-      </div>
+        <div className="right-half">
+          <article>
+          <Form>
+            <Label className="text">Please Sign In</Label>
+            <Input label="Email"/>
+            <Input label="Password" type="password"/>
+            <br/>
+            <Button color="info" block>Sign In!</Button>
+          </Form>
+          <hr/>
+          <NavLink style={{textDecoration: 'none'}} to="/create-account">
+            <Button color="info" block>Sign Up!</Button>
+          </NavLink>
+          </article>
+        </div>
+      </section>
     );
   }
 }
 
 export default App;
+
+/*
+      <div className='signIn'>
+        <div className='signInImage' >
+          <img src={blueImage} alt="Blue Blob"/>
+        </div>
+        <div className='signInForm' >
+          <Form>
+            <Label className="text">Please Sign In</Label>
+            <Input label="Email"/>
+            <Input label="Password" type="password"/>
+            <br/>
+            <Button color="info" block>Sign In!</Button>
+          </Form>
+          <hr/>
+          <NavLink style={{textDecoration: 'none'}} to="/create-account">
+            <Button color="info" block>Sign Up!</Button>
+          </NavLink>
+        </div>
+      </div>
+ */
