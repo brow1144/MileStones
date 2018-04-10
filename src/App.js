87a7e1baee6cda28209e65c31a7b45ecb05d76f5
Kyle Brown <brow1144@purdue.edu>
2018-04-10 15:55:48 -0400

    Sign In Page complete with logo

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
 
-import {Input, Button, Card, CardImage, CardBody, CardTitle, CardText} from 'mdbreact';
-import {Form} from 'reactstrap';
+import SignIn from './SignIn/SignIn';
+import CreateUser from './CreateUser/CreateUser';
 
 class App extends Component {
   render() {
     return (
-      <section className="container">
-        <div className="left-half" />
-        <div className="right-half">
-          <article>
-            <h3 className="text">Please Sign In</h3>
-            <Form>
-              <Input style={{fontSize: '0.75em'}} label="Email"/>
-              <Input label="Password" type="password"/>
-              <br/>
-              <Button className='signInButton' color="info" >Sign In!</Button>
-            </Form>
-            {/*<hr/>*/}
-            {/*<NavLink style={{textDecoration: 'none'}} to="/create-account">*/}
-            {/*<Button color="info" >Sign Up!</Button>*/}
-          {/*</NavLink>*/}
-          </article>
-        </div>
-      </section>
+      <SignIn />
     );
   }
 }
