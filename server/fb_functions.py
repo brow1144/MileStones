3de73b224b5220747925bf00bd43c4eb71418bba
Walter Jacquette <wjacquet@purdue.edu>
2018-04-16 18:12:12 -0400

    More firebase functions

--- server/fb_functions.py
+++ server/fb_functions.py
@@ -1,12 +1,14 @@
-
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import random
 
 # initialize firebase
 cred = credentials.Certificate('./milestones-firebase-sdk.json')
 default_app = firebase_admin.initialize_app(cred) 
 db = firestore.client()
+
+
 
 def addUser(user):
     userRef = db.collection('users').document(str(user.ID))
@@ -17,6 +19,38 @@ def addUser(user):
 def getUsers():
     usersRef = db.collection('users').get()
     return usersRef
-    
+
+def idGenerator():
+    id = ""
+    for i in range(6):
+        id += random.randint(1,10)
+    return id
+
 def addProject(user,project):
     projectRef = db.collection('users').document(str(user.ID)).collection('projects').document(str(project.ID))
+    projectRef.set({
+        u'name': project.name,
+        u'completed': False,
+        u'dueDate': project.dueDate,
+        u'mileStones': [],
+    })
+
+def getAllProjects(uid):
+    projectsRef = db.collection('users').document(uid).collection('projects').get()
+    return projectsRef
+
+def addMileStone(uid,pid,mileStone):
+    print('yes')
+
+def getMilestones(uid):
+    projectRef = db.collection('users').document(uid).collection('projects').get()
+    stones = []
+    for doc in projectRef:
+        for ms in doc.mileStones:
+            stones.append(ms) 
+    return stones
+
+def getMilestonesByProject(uid,pid):
+    milesRef = db.collection('users').document(uid).collection('project').document(pid)
+    return milesRef
+
