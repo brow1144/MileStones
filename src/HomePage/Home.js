1f57d6e03fda9aecb4b9e550066a56508de32752
Kyle Brown <brow1144@purdue.edu>
2018-04-22 13:41:38 -0400

    Minor deleting

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import {Button, Navbar, NavbarBrand, Collapse, NavbarNav,
-  NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu,
-  DropdownItem, NavbarToggler, ListGroup, ListGroupItem} from 'mdbreact';
+import {Navbar, NavbarBrand, Collapse, NavbarNav,
+  NavItem, NavLink, NavbarToggler, ListGroup, ListGroupItem} from 'mdbreact';
 
 import {Row, Col} from 'reactstrap';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
-
-import Logo from '../logo.svg';
 
 import './Home.css';
 import 'font-awesome/css/font-awesome.min.css'
@@ -32,12 +29,12 @@ class Home extends Component {
   }
 
   componentWillMount() {
-    // axios.get('http://localhost:8082')
-    //   .then(
-    //     response => console.log(response)
-    //   ).catch(() => {
-    //   console.log('Good catch!')
-    // })
+    axios.get('http://localhost:5000/users')
+      .then(
+        response => console.log(response)
+      ).catch(() => {
+      console.log('Good catch!')
+    })
   }
 
   onClick = () => {
@@ -130,7 +127,7 @@ class Home extends Component {
                   <p className="mb-1">Make sure to work on the third page today!</p>
                   <small>Due 4/25/18</small>
                 </ListGroupItem>
-                <ListGroupItem hover href="#">
+                <ListGroupItem href="#">
                   <div className="d-flex w-100 justify-content-between">
                     <h5 className="mb-1">MA 162 Exam</h5>
                     <small className="text-muted">5 days to go</small>
@@ -138,7 +135,7 @@ class Home extends Component {
                   <p className="mb-1">Make sure to study chapters 5-6 today!</p>
                   <small className="text-muted">Due 04/27/18</small>
                 </ListGroupItem>
-                <ListGroupItem hover href="#">
+                <ListGroupItem href="#">
                   <div className="d-flex w-100 justify-content-between">
                     <h5 className="mb-1">CS 250 Lab 6</h5>
                     <small className="text-muted">6 days ago</small>
@@ -146,7 +143,7 @@ class Home extends Component {
                   <p className="mb-1">Make sure to work on part 1 of lab 6 today!</p>
                   <small className="text-muted">Due 04/28/18</small>
                 </ListGroupItem>
-                <ListGroupItem hover href="#">
+                <ListGroupItem href="#">
                   <div className="d-flex w-100 justify-content-between">
                     <h5 className="mb-1">CS 251 Project 3</h5>
                     <small className="text-muted">12 days to go</small>
