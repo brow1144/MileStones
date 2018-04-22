f925bc25ab3d8719f77e7052d0ccb844ae108396
Kyle Brown <brow1144@purdue.edu>
2018-04-22 12:02:28 -0400

    UI Hard Coded Text

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -76,7 +76,7 @@ class Home extends Component {
     const events = [
       {
         id: 14,
-        title: 'Today',
+        title: 'ENGL 106 Essay',
         start: new Date(new Date().setHours(new Date().getHours() - 3)),
         end: new Date(new Date().setHours(new Date().getHours() + 3)),
       },
@@ -123,36 +123,36 @@ class Home extends Component {
             <div className="sideFloat z-depth-2">
               <ListGroup>
                 <ListGroupItem href="#">
-                  <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">List group item heading</h5>
-                    <small>3 days ago</small>
+                  <div className="d-flex w-100 justify-content-between">
+                    <h5 className="mb-1">ENGL 106 Paper</h5>
+                    <small>3 days to go</small>
                   </div>
-                  <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                  <small>Donec id elit non mi porta.</small>
+                  <p className="mb-1">Make sure to work on the third page today!</p>
+                  <small>Due 4/25/18</small>
                 </ListGroupItem>
                 <ListGroupItem hover href="#">
-                  <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">List group item heading</h5>
-                    <small class="text-muted">3 days ago</small>
+                  <div className="d-flex w-100 justify-content-between">
+                    <h5 className="mb-1">MA 162 Exam</h5>
+                    <small className="text-muted">5 days to go</small>
                   </div>
-                  <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                  <small class="text-muted">Donec id elit non mi porta.</small>
+                  <p className="mb-1">Make sure to study chapters 5-6 today!</p>
+                  <small className="text-muted">Due 04/27/18</small>
                 </ListGroupItem>
                 <ListGroupItem hover href="#">
-                  <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">List group item heading</h5>
-                    <small class="text-muted">3 days ago</small>
+                  <div className="d-flex w-100 justify-content-between">
+                    <h5 className="mb-1">CS 250 Lab 6</h5>
+                    <small className="text-muted">6 days ago</small>
                   </div>
-                  <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                  <small class="text-muted">Donec id elit non mi porta.</small>
+                  <p className="mb-1">Make sure to work on part 1 of lab 6 today!</p>
+                  <small className="text-muted">Due 04/28/18</small>
                 </ListGroupItem>
                 <ListGroupItem hover href="#">
-                  <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">List group item heading</h5>
-                    <small class="text-muted">3 days ago</small>
+                  <div className="d-flex w-100 justify-content-between">
+                    <h5 className="mb-1">CS 251 Project 3</h5>
+                    <small className="text-muted">12 days to go</small>
                   </div>
-                  <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                  <small class="text-muted">Donec id elit non mi porta.</small>
+                  <p className="mb-1">Make sure to work on part 3 of the project!</p>
+                  <small className="text-muted">Due 5/01/18</small>
                 </ListGroupItem>
               </ListGroup>
             </div>
