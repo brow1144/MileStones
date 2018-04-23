ddb4d35408c699d930a9163b7b90769231756b4d
Kyle Brown <brow1144@purdue.edu>
2018-04-23 12:28:36 -0400

    Changed object set up!

--- src/Objects.js
+++ src/Objects.js
@@ -1,23 +1,29 @@
-// Milestone Object
-export function MileStone(id, name, dueDate) {
-    this.id = id;
-    this.name = name;
-    this.dueDate = dueDate; // date
-    this.completed = completed; // boolean
+// Project Object
+export class Project {
+    constructor(id, name, mileStones, dueDate, completed) {
+        this.id = id;
+        this.name = name;
+        this.mileStones = mileStones; // Array of Milestones
+        this.dueDate = dueDate; // date
+        this.completed = completed; //boolean
+    }
 }
 
-// Project Object
-export function Project(id, name, mileStones, dueDate, completed) {
-    this.id = id;
-    this.name = name;
-    this.mileStones = mileStones; // Array of Milestones
-    this.dueDate = dueDate; // date
-    this.completed = completed; //boolean
+// Milestone Object
+export class MileStone {
+    constructor(id, name, dueDate, completed) {
+        this.id = id;
+        this.name = name;
+        this.dueDate = dueDate; // date
+        this.completed = completed; // boolean
+    }
 }
 
 // User Object
-export function User(id, name, projects) {
-    this.id;
-    this.name = name;
-    this.projects = projects; // Array of Projects
+export class User {
+    constructor(id, name, projects) {
+        this.id;
+        this.name = name;
+        this.projects = projects; // Array of Projects
+    }
 }
