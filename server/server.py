be2555201427ad09bf16d2537b45edf9c2779bf9
Walter Jacquette <wjacquet@purdue.edu>
2018-04-26 11:12:34 -0400

    Initial Deployment

--- server/server.py
+++ server/server.py
@@ -4,9 +4,12 @@ import fb_functions as fb
 import objects as obj
 from flask_cors import CORS
 import sys
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+port = int(os.getenv('PORT', 8000))
 
 @app.route("/")
 def hello_world():
@@ -104,3 +107,6 @@ def update_project():
     newProject = obj.Project(projectJson['id'], projectJson['name'], projectJson['dueDate'], projectJson['mileStones'], projectJson['completed'], projectJson['hidden'])
     fb.updateProject(newUser, newProject)
     return '', 204
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, debug=True)
